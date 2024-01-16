b4e121782a9f0c2a6820639a297653090695fb95
VasoyaDhruv <dhruvvasoya48@gmail.com>
2024-01-17 00:03:20 +0530

    "testimonial section"

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-console.log("hello");
-console.log("hello");
-console.log("hello");
-console.log("hello");
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll",() => {
+  if(window.scrollY > 60)  {
+    nav.classList.add("scrolled");
+  
+  }else(
+    nav.classList.remove("scrolled")
+  )
+})

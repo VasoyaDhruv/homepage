951acbb1299e3025a51984a97051b7f011875579
VasoyaDhruv <dhruvvasoya48@gmail.com>
2024-01-17 23:55:43 +0530

    "added tablet responsive"

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const nav = document.querySelector("nav");
+const mobilenav = document.querySelector('nav.mobile-nav');
+const menuContainer = document.querySelector(".mobile-menu-container");
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".mobile-menu-container .close-icon");
+
 window.addEventListener("scroll",() => {
   if(window.scrollY > 60)  {
     nav.classList.add("scrolled");
-  
-  }else(
+    mobilenav.classList.add("scrolled")
+  }else{
     nav.classList.remove("scrolled")
-  )
+    mobilenav.classList.remove("scrolled")
+  }
 })
+
+menuIcon.addEventListener("click",() => {
+  menuContainer.classList.add("active");
+})
+
+closeIcon.addEventListener("click",() => {
+  menuContainer.classList.remove("active");
+})
+
+
+
